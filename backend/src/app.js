765bbcfccb127982eb98c1061904e71d61659294
ajay singh <kiyomito8173@gmail.com>
2026-01-03 14:01:07 +0530

    Set up Express app with MongoDB connection and routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,22 @@
-// Express app setup will go here
+const express = require("express");
+const connectDB = require("./config/db");
+const sosRoutes = require("./routes/sos.routes");
+
+const app = express();
+
+// Connect MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api", sosRoutes);
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("SOS Backend is running");
+});
+
+module.exports = app;
+
