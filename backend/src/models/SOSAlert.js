82a9087c29cf98141c5eb69fbd5bc4f2a12cd2d7
ajay singh <kiyomito8173@gmail.com>
2026-01-03 14:03:34 +0530

    Define SOSAlert schema with Mongoose

--- backend/src/models/SOSAlert.js
+++ backend/src/models/SOSAlert.js
@@ -1,1 +1,20 @@
-// SOS alert schema will go here
+// SOS alert schema will go hereconst mongoose = require("mongoose");
+
+const SOSAlertSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true
+    },
+    latitude: Number,
+    longitude: Number,
+    status: {
+      type: String,
+      default: "sent"
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("SOSAlert", SOSAlertSchema);
+
