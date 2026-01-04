4cab3687ee073b9660da9a65684c5cf5efcf49bc
ajay singh <kiyomito8173@gmail.com>
2026-01-04 12:03:12 +0530

    Add EmergencyContact schema using Mongoose

--- backend/src/models/EmergencyContact.js
+++ backend/src/models/EmergencyContact.js
@@ -1,1 +1,28 @@
 // Emergency contact schema will go here
+const mongoose = require("mongoose");
+
+const EmergencyContactSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    phone: {
+      type: String,
+      required: true
+    },
+    relation: {
+      type: String
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model(
+  "EmergencyContact",
+  EmergencyContactSchema
+);
